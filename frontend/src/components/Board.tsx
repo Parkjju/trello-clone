bf443e08103eee153cafce93ca19927558bd99ac
Parkjju <rudwns3927@gmail.com>
2022-06-14 05:16:53 +0900

    ✨ feat: todo리스트 제작 마무리 / dragging이벤트 등

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,13 +1,15 @@
+import { useRef } from 'react';
 import { Droppable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 import DraggableCard from './DraggableCard';
 
 const Wrapper = styled.div`
-  padding: 20px 10px;
   padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 200px;
+  display: flex;
+  flex-direction: column;
 `;
 
 interface IProps {
@@ -25,18 +27,45 @@ const Title = styled.div`
   margin-bottom: 5px;
 `;
 
+interface IAreaProps {
+  isDraggingOver: boolean;
+  isDraggingFromThis: boolean;
+}
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver
+      ? '#dfe6e9'
+      : props.isDraggingFromThis
+      ? '#b2bec3'
+      : '#dfe6e9'};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
+  padding: 20px;
+`;
+
 function Board({ toDos, boardId }: IProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClick = () => {
+    inputRef.current?.focus();
+  };
   return (
     <Wrapper>
+      <input placeholder='focus me' ref={inputRef} />
+      <button onClick={onClick}>Click me</button>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(magic) => (
-          <div {...magic.droppableProps} ref={magic.innerRef}>
+        {(magic, snapshot) => (
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+            {...magic.droppableProps}
+            ref={magic.innerRef}
+          >
             {toDos.map((toDo, index) => (
               <DraggableCard key={toDo} index={index} item={toDo} />
             ))}
             {magic.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
--- frontend/src/components/DraggableCard.tsx
+++ frontend/src/components/DraggableCard.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
   margin-bottom: 5px;
   padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
+  background-color: ${(props) =>
+    props.isDragging ? '#74b9ff' : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.isDragging ? '0px 2px 5px rgba(0,0,0,0.05)' : 'none'};
 `;
 
 interface IProps {
@@ -17,11 +20,12 @@ interface IProps {
 function DraggableCard({ item, index }: IProps) {
   return (
     <Draggable draggableId={item} index={index}>
-      {(magic) => (
+      {(magic, snapshot) => (
         <Card
           {...magic.dragHandleProps}
           {...magic.draggableProps}
           ref={magic.innerRef}
+          isDragging={snapshot.isDragging}
         >
           {item}
         </Card>
